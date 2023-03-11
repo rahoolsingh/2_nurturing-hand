ea110d5633895191ce253b8ee415293ed6544078
Rahul Kumar Singh <rahulksingh3907@gmail.com>
2023-03-11 13:54:30 +0530

    blog

--- blog/blog.js
+++ blog/blog.js
@@ -1,35 +1,49 @@
-// Load the Google API client library script
-(function() {
-    var script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.src = 'https://apis.google.com/js/client.js';
-  
-    // Once the script has loaded, fetch the blog data
-    script.onload = function() {
-      // Initialize the API client library
-      gapi.client.init({
-        apiKey: 'YOUR_API_KEY',
-        discoveryDocs: ['https://www.googleapis.com/discovery/v1/apis/blogger/v3/rest']
-      }).then(function() {
-        // Fetch the blog data
-        gapi.client.blogger.blogs.get({
-          blogId: 'BLOG_ID'
-        }).then(function(response) {
-          // Extract the blog name, image, and description from the response
-          var blog = response.result;
-          var blogName = blog.name;
-          var blogImage = blog.image.url;
-          var blogDescription = blog.description;
-  
-          // Do something with the blog data
-          console.log('Blog Name:', blogName);
-          console.log('Blog Image:', blogImage);
-          console.log('Blog Description:', blogDescription);
+
+  const apiKey = 'AIzaSyAixrPHiqKtHpbBEto7XQPOvJVV8OBoaYM';
+  const blogId = '6178294570622619126';
+  const url = `https://www.googleapis.com/blogger/v3/blogs/${blogId}/posts?key=${apiKey}`;
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      if (data.items) {
+        data.items.forEach(item => {
+          const blogTitle = item.title;
+          var blogDescription = item.content.replace(/<[^>]+>/g, '');
+          if (blogDescription.length > 250){
+            blogDescription = blogDescription.substring(0, 249) + "...";
+          }
+          var blogDate = new Date(item.published).getDate();
+          if (blogDate < 10){
+            blogDate = '0' + String(blogDate);
+          }
+          const blogMonth = new Date(item.published).toLocaleString('default', { month: 'short' });
+          const blogComments = item.replies.totalItems;
+          
+          const blogHTML = `
+            <article class="blog_item">
+              <div class="blog_item_img">
+                <a href="#" class="blog_item_date">
+                  <h3>${blogDate}</h3>
+                  <p>${blogMonth}</p>
+                </a>
+              </div>
+              <div class="blog_details">
+                <a class="d-inline-block" href="#">
+                  <h2 class="blog-head" style="color: #2d2d2d">${blogTitle}</h2>
+                </a>
+                <p class='blog-main-description'>${blogDescription}</p>
+                <ul class="blog-info-link">
+                  <li><a href="#"><i class="fa fa-user"></i></a></li>
+                  <li><a href="#"><i class="fa fa-comments"></i>${blogComments} Comments</a></li>
+                </ul>
+              </div>
+            </article>
+          `;
+          
+          document.querySelector('#blog_section').insertAdjacentHTML('beforeend', blogHTML);
         });
-      });
-    };
-  
-    // Add the script to the document
-    document.head.appendChild(script);
-  })();
-  
+        console.log(data.items);
+      }
+    })
+    .catch(error => console.error(error));
